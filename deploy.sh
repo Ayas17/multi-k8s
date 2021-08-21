ea0dd667c6506a812577b308a982a1f811bd5ad3
ayandamkula <ayanda@opencartplugins.net>
2021-08-21 15:04:55 +0200

    More changes fixed a typo

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push mkulaa1/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=mkulaa1/multi-server:$SHA
-kubectl set image deployments/client-deployment client=mkulaa1/client-server:$SHA
+kubectl set image deployments/client-deployment client=mkulaa1/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=mkulaa1/multi-worker:$SHA
